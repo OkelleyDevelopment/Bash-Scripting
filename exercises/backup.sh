d80752367566e80ac447f30714015358c8c26f5c
Nicholas O'Kelley <okelleynp@gmail.com>
2022-11-30 14:47:10 -0500

    fix: Add more context to my backup script

--- exercises/backup.sh
+++ exercises/backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-OF="my-backup-$(date +%Y%m%d).tbz"
-
-tar -cjf $OF "./" 
+archive="my-backup-$(date +%Y-%m-%d).tbz"
+tar -cvjf $archive $1
+echo "archive named: " $archive
+echo "Done."
